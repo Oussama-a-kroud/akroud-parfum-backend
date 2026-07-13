1b061187eab275db9b8b17788cd5431c7d2dac5e
Oussama Akroud <oussamaakroud82@gmail.com>
2026-07-13 19:38:00 +0100

    Fix web.php route

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Had l-code kay-goul l-Laravel: 
-// "Ay lien y-dkhl lih l-klyan (ghir ila kan m-bdi b /api), wrih l-fichier index.html"
-
-Route::get('/{any}', function () {
-    return file_get_contents(public_path('index.html'));
-})->where('any', '^(?!api).*$');
+Route::get('/', function () {
+    return response()->json(['message' => 'Akroud Parfum API is working perfectly! 🚀']);
+});

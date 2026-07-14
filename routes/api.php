d8f95ebdcbcdd69ebc010f6903602a6ac2a06fd8
Oussama Akroud <oussamaakroud82@gmail.com>
2026-07-14 01:08:56 +0100

    Fix API routes for perfumes and commandes

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PerfumeController;
 use App\Http\Controllers\Api\CommandeController;
 
+// الرسالة باش ديما تيستي واش السيرفر خدام
+Route::get('/', function () {
+    return response()->json(['message' => 'Akroud Parfum API is working perfectly! 🚀']);
+});
+
 // L'adresse API bach njibo la liste d rway7
 Route::get('/perfumes', [PerfumeController::class, 'index']);
-// Route::post('/commandes', [CommandeController::class, 'store']);
-Route::post('/commandes', [App\Http\Controllers\Api\CommandeController::class, 'store']);
-Route::post('/perfumes', [App\Http\Controllers\Api\PerfumeController::class, 'store']);
-// La route jdida li katzid rway7 (Zid hadi)
+
+// La route jdida li katzid rway7 
 Route::post('/perfumes', [PerfumeController::class, 'store']);
+
+// Route pour les commandes
+Route::post('/commandes', [CommandeController::class, 'store']);

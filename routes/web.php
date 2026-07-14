3e9830b61288593f8ce1b6cf3a07bbc0b300c99f
Oussama Akroud <oussamaakroud82@gmail.com>
2026-07-14 01:56:26 +0100

    Fix API routes for perfumes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return response()->json(['message' => 'Akroud Parfum API is working perfectly! 🚀']);
 });
+
+// فـ ملف routes/web.php
+Route::get('perfumes', [App\Http\Controllers\PerfumeController::class, 'index']);
+Route::post('perfumes', [App\Http\Controllers\PerfumeController::class, 'store']);
+Route::post('commandes', [App\Http\Controllers\Api\CommandeController::class, 'store']);
